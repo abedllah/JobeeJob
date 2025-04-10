3a580480ee21fbe51603b03815c339f6597c75a6
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2025-04-10 20:01:21 +0100

    feat: implement CommunityPage and restructure App component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Navbar from './Component/Navbar';
-import CreatePost from './Component/CreatePost';
-import JobListings from './Component/JobListings';
-import RightBowl from './Component/RightBowl'
-import LeftBowl from './Component/LeftBowl'
+import CommunityPage from "./pages/CommunityPage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-    <div className="flex gap-4 p-4 max-w-7xl mx-auto">
-      {/* Colonne gauche - Bowl des suggestions (20%) */}
-      <div className="w-1/5">
-        <LeftBowl />
-      </div>
-      
-      {/* Colonne centrale - Contenu principal (60%) */}
-      <div className="w-3/5 space-y-4">
-        <CreatePost />
-        <JobListings />
-      </div>
-      
-      {/* Colonne droite - Bowl des pubs/trends (20%) */}
-      <div className="w-1/5">
-        <RightBowl />
-      </div>
-    </div>
+      <CommunityPage />
     </div>
   );
 }
